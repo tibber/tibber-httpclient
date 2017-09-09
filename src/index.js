c575b47d7a5a8f5246e2fd86e0c8212b21a743ff
Kjell Inge Rotnes <kirotnes@gmail.com>
2017-09-09 10:51:07 +0200

    Moved to typescript

--- src/index.js
+++ src/index.js
@@ -1,120 +1,126 @@
-import request from 'request-promise';
-import moment from 'moment';
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import * as request from "request-promise";
+import * as moment from "moment";
 class WrapperLogger {
-    constructor(logger){
-        this.logger = logger||{};
+    constructor(logger) {
+        this.logger = logger || {};
     }
-    info() { 
-
-      this.logger.info && this.logger.info(...arguments);
+    info() {
+        this.logger.info && this.logger.info(arguments).apply(this.logger, arguments);
     }
-    error() { 
-        this.logger.error && this.logger.error(...arguments);
+    error() {
+        this.logger.error && this.logger.error(arguments).apply(this.logger, arguments);
     }
-    debug() { 
-        this.logger.debug && this.logger.debug(...arguments);
+    debug() {
+        this.logger.debug && this.logger.debug.apply(this.logger, arguments);
     }
 }
-
 export class HttpClient {
-
     constructor(baseUrl, logger, basicAuthUser, basicAuthPwd) {
-
         if (!baseUrl)
-            throw new Error('baseUrl must be defined');
-
+            throw new Error("baseUrl must be defined");
         this._baseUrl = baseUrl;
         this._logger = new WrapperLogger(logger);
-
         if (basicAuthUser && basicAuthPwd) {
             this._defaultHeaders = {
                 "Authorization": "Basic " + new Buffer(basicAuthUser + ":" + basicAuthPwd).toString("base64")
-            }
+            };
         }
     }
-
-    async _request(method, path, body) {
-
-        const url = `${this._baseUrl}${path}`;
-        const start = moment();        
-        let sw = {};
-        const options = {
-            method: method,
-            uri: url,
-            body: body,
-            headers: this._defaultHeaders,
-            resolveWithFullResponse: true
-        };
-        try {
-            const result = await request(options);
-            this._logger.info(`${method} ${url} ${result.statusCode} (${moment().diff(start,'milliseconds')} ms)`);
-            this._logger.debug('request-options', options);
-            
-            return result.body ? JSON.parse(result.body): undefined;            
-        }
-        catch (error) {
-         
-            let message = null;
-            if (error.response && error.response.errors && error.response.errors.length > 0) {
-                message = error.response.errors.join(",");
+    _request(method, path, body) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const url = `${this._baseUrl}${path}`;
+            const start = moment();
+            const options = {
+                method: method,
+                uri: url,
+                body: body,
+                headers: this._defaultHeaders,
+                resolveWithFullResponse: true
+            };
+            try {
+                const result = yield request(options);
+                this._logger.info(`${method} ${url} ${result.statusCode} (${moment().diff(start, "milliseconds")} ms)`);
+                this._logger.debug("request-options", options);
+                return result.body ? JSON.parse(result.body) : undefined;
             }
-            else {
-                message = error.toString && error.toString();
+            catch (error) {
+                let message = "";
+                if (error.response && error.response.errors && error.response.errors.length > 0) {
+                    message = error.response.errors.join(",");
+                }
+                else {
+                    message = error.toString && error.toString();
+                }
+                this._logger.error("--------------------------------------------------------------------\n"
+                    + `${method} ${url} ${error.response && error.response.statusCode || "<unknown statuscode>"} (${moment().diff(start, "milliseconds")} ms)\n`
+                    + `request-options: ${JSON.stringify(options)}\n`
+                    + `error:${message}\n` + "--------------------------------------------------------------------");
+                throw error;
             }
-            this._logger.error('--------------------------------------------------------------------\n'
-                                +`${method} ${url} ${error.response && error.response.statusCode || '<unknown statuscode>'} (${moment().diff(start,'milliseconds')} ms)\n`
-                                +`request-options: ${JSON.stringify(options)}\n`
-                                +`error:${message}\n`+'--------------------------------------------------------------------');
-            throw error;
-        }
+        });
     }
-
-    async get(route) {
-        return await this._request('GET', route);
+    get(route) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this._request("GET", route);
+        });
     }
-
-    async post(route, payload) {
-        return await this._request('POST', route, payload);
+    post(route, payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this._request("POST", route, payload);
+        });
     }
-
-    async patch(route, payload) {
-        return await this._request('PATCH', route, payload);
+    patch(route, payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return (yield this._request("PATCH", route, payload));
+        });
     }
-
-    async delete(route) {
-        return await this._request('DELETE', route);
+    put(route, payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this._request("PATCH", route, payload);
+        });
+    }
+    delete(route) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield this._request("DELETE", route);
+        });
     }
 }
-
 export class TestHttpClient {
-
     constructor(routePayloads) {
         this._routePayloads = routePayloads;
         this.calls = { get: {}, post: {}, patch: {}, delete: {} };
     }
-
-    async get(route) {
-
-        this.calls.get[route] = {};
-        let result = this._routePayloads.get[route];
-
-        return this._routePayloads.get[route];
+    get(route) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.calls.get[route] = {};
+            const result = this._routePayloads.get[route];
+            return this._routePayloads.get[route];
+        });
     }
-
-    async post(route, payload) {
-        this.calls.post[route] = payload || {};
-        return this._routePayloads.post[route];
+    post(route, payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.calls.post[route] = payload || {};
+            return this._routePayloads.post[route];
+        });
     }
-
-    async patch(route, payload) {
-        this.calls.patch[route] = payload || {};
-        return this._routePayloads.patch[route];
+    patch(route, payload) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.calls.patch[route] = payload || {};
+            return this._routePayloads.patch[route];
+        });
     }
-
-    async delete(route) {
-        this.calls.delte[route] = null;
-        return this._routePayloads.delete[route];
+    delete(route) {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.calls.delete[route] = undefined;
+            return this._routePayloads.delete[route];
+        });
     }
 }
-
---
+++ src/index.ts
@@ -0,0 +1,137 @@
+import * as request from "request-promise";
+import * as moment from "moment";
+
+export interface Logger {
+    info?(...any);
+    error?(...any);
+    debug?(...any);
+}
+
+class WrapperLogger {
+    private logger: Logger;
+    constructor(logger?: Logger) {
+        this.logger = logger || {};
+    }
+    public info() {
+
+        this.logger.info && this.logger.info.apply(this.logger, arguments);
+    }
+    public error() {
+        this.logger.error && this.logger.error.apply(this.logger, arguments);
+    }
+    public debug() {
+        this.logger.debug && this.logger.debug.apply(this.logger, arguments);
+    }
+}
+
+type HTTP_METHOD = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+export class HttpClient {
+
+    private _baseUrl: string;
+    private _logger: Logger;
+    private _defaultHeaders: any;
+    constructor(baseUrl: string, logger?: Logger, basicAuthUser?: string, basicAuthPwd?: string) {
+
+        if (!baseUrl)
+            throw new Error("baseUrl must be defined");
+
+        this._baseUrl = baseUrl;
+        this._logger = new WrapperLogger(logger);
+
+        if (basicAuthUser && basicAuthPwd) {
+            this._defaultHeaders = {
+                "Authorization": "Basic " + new Buffer(basicAuthUser + ":" + basicAuthPwd).toString("base64")
+            };
+        }
+    }
+
+    private async _request(method: HTTP_METHOD, path: string, body?: object) {
+
+        const url = `${this._baseUrl}${path}`;
+        const start = moment();
+
+        const options = {
+            method: method,
+            uri: url,
+            body: body,
+            headers: this._defaultHeaders,
+            resolveWithFullResponse: true
+        };
+        try {
+            const result = await request(options);
+            this._logger.info(`${method} ${url} ${result.statusCode} (${moment().diff(start, "milliseconds")} ms)`);
+            this._logger.debug("request-options", options);
+
+            return result.body ? JSON.parse(result.body) : undefined;
+        }
+        catch (error) {
+
+            let message = "";
+            if (error.response && error.response.errors && error.response.errors.length > 0) {
+                message = error.response.errors.join(",");
+            }
+            else {
+                message = error.toString && error.toString();
+            }
+            this._logger.error("--------------------------------------------------------------------\n"
+                + `${method} ${url} ${error.response && error.response.statusCode || "<unknown statuscode>"} (${moment().diff(start, "milliseconds")} ms)\n`
+                + `request-options: ${JSON.stringify(options)}\n`
+                + `error:${message}\n` + "--------------------------------------------------------------------");
+            throw error;
+        }
+    }
+
+    public async get<T>(route): Promise<T> {
+        return await this._request("GET", route);
+    }
+
+    public async post<T>(route, payload): Promise<T> {
+        return await this._request("POST", route, payload);
+    }
+
+    public async patch<T>(route, payload): Promise<T> {
+        return (await this._request("PATCH", route, payload));
+    }
+
+    public async put<T>(route, payload): Promise<T> {
+        return await this._request("PATCH", route, payload);
+    }
+
+    public async delete(route) {
+        return await this._request("DELETE", route);
+    }
+}
+
+export class TestHttpClient {
+    public _routePayloads: any;
+    public calls: any;
+
+    constructor(routePayloads) {
+        this._routePayloads = routePayloads;
+        this.calls = { get: {}, post: {}, patch: {}, delete: {} };
+    }
+
+    public async get(route) {
+        this.calls.get[route] = {};
+        const result = this._routePayloads.get[route];
+
+        return this._routePayloads.get[route];
+    }
+
+    public async post(route, payload) {
+        this.calls.post[route] = payload || {};
+        return this._routePayloads.post[route];
+    }
+
+    public async patch(route, payload) {
+        this.calls.patch[route] = payload || {};
+        return this._routePayloads.patch[route];
+    }
+
+    public async delete(route) {
+        this.calls.delete[route] = undefined;
+        return this._routePayloads.delete[route];
+    }
+}
+
